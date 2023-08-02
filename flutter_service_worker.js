8aab1e50b2760a6d9eee0a05a173d5d4e1213e69
Rukmini Sahani <rukminisahani@gmail.com>
2023-08-02 15:03:00 +0530

    build server

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -306,9 +306,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "9f7619b89d0c25a9c652f623136c7448",
-"/": "9f7619b89d0c25a9c652f623136c7448",
-"main.dart.js": "69d7a7a0f9980d6747ec1cf3586881bb",
+"index.html": "ffb3aa66a6c09d1fb0fcaf3d9b5118f7",
+"/": "ffb3aa66a6c09d1fb0fcaf3d9b5118f7",
+"main.dart.js": "51466788598d55ec15bc4c9c6a5cd10b",
 "manifest.json": "0e8479a722cb05d280e19a40fbb0ffc1",
 "version.json": "74f02d96d4f0cce19b549519ea24cb47"
 };
