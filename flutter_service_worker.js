1c9caf720c3a9d6d83d24a50930c081518006373
madhubanpietech <madhuban@pietechsolution.com>
2023-08-09 16:26:12 +0530

    pushed

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,10 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "74f02d96d4f0cce19b549519ea24cb47",
-"index.html": "46bcaba75a91c3749751dceb45174221",
-"/": "46bcaba75a91c3749751dceb45174221",
-"main.dart.js": "02d86b5649a0b9124abdecfc6dff9575",
-"flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
+"index.html": "3d8e7835c98dc625ecc68ca710fe655d",
+"/": "3d8e7835c98dc625ecc68ca710fe655d",
+"main.dart.js": "255ee74fccd57ae5e5bbc6223780b100",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
@@ -306,11 +305,7 @@ const RESOURCES = {
 "assets/assets/files/logodrawer.png": "fe2ce64657b6576db4b1e31372e7920d",
 "assets/assets/fonts/Poppins/Poppins-Light.ttf": "fcc40ae9a542d001971e53eaed948410",
 "assets/assets/fonts/Poppins/Poppins-Medium.ttf": "bf59c687bc6d3a70204d3944082c5cc0",
-"assets/assets/fonts/Poppins/Poppins-Bold.ttf": "08c20a487911694291bd8c5de41315ad",
-"canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
-"canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
-"canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
-"canvaskit/canvaskit.wasm": "3de12d898ec208a5f31362cc00f09b9e"
+"assets/assets/fonts/Poppins/Poppins-Bold.ttf": "08c20a487911694291bd8c5de41315ad"
 };
 
 // The application shell files that are downloaded before a service worker can
